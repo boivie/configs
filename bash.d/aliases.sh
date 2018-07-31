4217edf75391343c5f78f8eb44524d960f84aa2a
Victor Boivie <victor@boivie.com>
2018-07-31 10:19:46 +0200

    Updated aliases
    Change-Id: I4fe515c90720a2c879c2a685e3e661ecf88c5a00

--- bash.d/aliases.sh
+++ bash.d/aliases.sh
@@ -4,9 +4,5 @@ alias la='ls -A'
 
 # Git
 alias g='git'
-alias gst='git st'
 alias gb='git branch'
 alias gba='git branch -a'
-
-# emacs
-alias em='/Applications/Emacs.app/Contents/MacOS/bin/emacsclient -n'
